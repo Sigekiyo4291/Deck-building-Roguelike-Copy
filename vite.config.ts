6a15f34f47ab15d837a88b367a71eeb609428442
fg-someya <yuki.someya@fujigames.jp>
2026-02-06 23:26:18 +0900

    feat:Build and deployment > Source を「GitHub Actions」に変更に伴う修正

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     // baseを './' に設定することで、デプロイ先のサブディレクトリ名に関わらず
     // 相対パスで資産を読み込むようになります。
-    base: './',
+    base: '/Deck-building-Roguelike-Copy/',
     build: {
         outDir: 'dist',
     },
